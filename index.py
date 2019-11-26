9749e6d9bcdf27eab7a39e9c0be6907c4a0e96ff
cdaket <cdaket@gmail.com>
2019-11-26 15:12:41 +0900

    いったん完成

--- index.py
+++ index.py
@@ -1,54 +1,45 @@
-import sys
 from urllib import request
-from urllib.request import urlopen
 from json import loads
 import slackweb
-
-# WEBAPIから遅延情報を取得
-body = request.urlopen('https://tetsudo.rti-giken.jp/free/delay.json').read()
-body.decode('utf-8')
-repos = loads(body)
-print(repos)
-
-# 遅延情報があったら処理を続行
-print(len(repos))
-if len(repos) == 0:
-    print('遅延情報なし')
-    sys.exit()
-
-# 仙台の遅延情報があったら処理を続行
-sendaiDelayData = []
-for train in repos:
-    if train['name'] == '東北本線':
-        print('東北本線')
-        sendaiDelayData.append(train)
-
-print(sendaiDelayData)
-
-# slackに投げるように文字を整形する
-if len(sendaiDelayData) == 0:
-    print('仙台の遅延情報なし')
-    sys.exit()
-
-attachments = []
-for data in sendaiDelayData:
-    attachment = {
-        'title': data['name'],
-        'title_link': 'title_link',
-        'color': 'good',
-        'author_icon': 'author_icon',
-        'author_name': 'author_name',
-        'footer': 'footer',
-        'ts': 'ts',
-    }
-    attachments.append(attachment)
-print(attachments)
-
-# slack設定
-webhook_url = ''
-slack = slackweb.Slack(url=webhook_url)
-
-# slackに投げる
-slack.notify(attachments=attachments)
+import config
 
 
+def main():
+    # WEBAPIから遅延情報を取得
+    body = request.urlopen('https://tetsudo.rti-giken.jp/free/delay.json').read()
+    body.decode('utf-8')
+    repos = loads(body)
+
+    # 遅延情報があったら処理を続行
+    if len(repos) == 0:
+        return
+
+    # 仙台の遅延情報があったら処理を続行
+    sendai_delay_data = []
+    for train in repos:
+        if train['name'] == '東北本線':
+            sendai_delay_data.append(train)
+
+    # slackに投げるように文字を整形する
+    if len(sendai_delay_data) == 0:
+        return
+
+    attachments = []
+    for data in sendai_delay_data:
+        attachment = {
+            'title': data['name'],
+            'title_link': 'https://traininfo.jreast.co.jp/train_info/tohoku.aspx',
+            'text': '詳細はURLから確認してください。',
+            'color': 'good',
+        }
+        attachments.append(attachment)
+
+    # slack設定
+    webhook_url = config.get_webhook_url()
+    slack = slackweb.Slack(url=webhook_url)
+
+    # slackに投げる
+    slack.notify(attachments=attachments)
+
+
+main()
